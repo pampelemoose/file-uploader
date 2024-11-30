25c07ecfe0804792a009c60de3405453479a7a1c
Maxime <zunkwo@gmail.com>
2024-11-30 12:30:45 +0700

    api-server - initiial commit - .NET Core API Project

---
+++ api-server/Controllers/FileUpload.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileUpload : ControllerBase
+    {
+
+    }
+}
